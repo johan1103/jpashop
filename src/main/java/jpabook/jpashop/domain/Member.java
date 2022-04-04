cebdace3a39ff878dcd51159895facd6777fc89e
kth1103 <johan1103@naver.com>
2022-04-04 21:33:42 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -19,6 +19,7 @@ public class Member {
     private Address address;
     //이런식으로 참조를 Member와 Orders가 서로 참조를 하면
     //외래키 변경을 할 때, 하나만 할 수 있도록 해야 하므로 연관관계의 주인에게만 정보를 주는게 맞음
-    @OneToMany(mappedBy = "member")
+    @OneToMany(mappedBy = "member",fetch = FetchType.LAZY)
     private List<Order> orderList = new ArrayList<>();
+
 }

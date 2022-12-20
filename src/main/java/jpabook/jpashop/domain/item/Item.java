daf7785ee16b1086375b87699a8481628cb03cb4
johan1103 <johan1103@naver.com>
2022-12-21 01:47:52 +0900

    Edit Item Add ItemRepository

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.CatergoryItem;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 
 import java.util.List;
@@ -24,4 +25,23 @@ public abstract class Item {
     private List<CatergoryItem> catergoryItems;
 
      */
+
+    //== 비즈니스 로직 추가 ==//
+
+    /**
+     * 객체지향의 관점에서 응집도를 낮추는 설계
+     * 필드값을 수정하는 주체는 필드값을 소유하고 있는 객체에게 역할 위임하는 것이 응집도를 낮추기에 좋음
+     */
+    public void AddStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
+
+    public void removeStock(int quantity){
+        int resStock=this.stockQuantity-quantity;
+        if(resStock<0){
+            throw new NotEnoughStockException ("need more stock");
+
+        }
+        this.stockQuantity=resStock;
+    }
 }

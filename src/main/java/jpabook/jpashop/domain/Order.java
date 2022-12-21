860d66feb48531ab6a3ce977b31df19dd3c8a420
johan1103 <johan1103@naver.com>
2022-12-21 23:39:11 +0900

    Edit Order,Edit OrderItem

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -27,7 +27,7 @@ public class Order {
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
-    private Status OrderStatus;
+    private Status orderStatus;
 
     //==연관관계 편의 메서드==//
     public void setMember(Member member){
@@ -38,8 +38,57 @@ public class Order {
         orderItem.setOrder(this);
         orderItems.add(orderItem);
     }
-
+    private void setOrderDate(){
+        this.orderDate=LocalDateTime.now();
+    }
+    private void setOrderStatus(Status status){
+        this.orderStatus=status;
+    }
     public void setDelivery(Delivery delivery){
         this.delivery=delivery;
     }
+
+    //==생성 메서드==//
+    public Order(Member member, Delivery delivery,OrderItem ... orderItems){
+        this.setMember(member);
+        this.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems){
+            this.addOrderItems(orderItem);
+        }
+        this.setOrderDate();
+        this.setOrderStatus(Status.ORDER);
+    }
+
+    /**
+     * 파라미터 없는 생성자 외부 사용 차단
+     */
+    protected Order(){
+    }
+
+    //== 비즈니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if(delivery.getStatus()==DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setOrderStatus(Status.CANCEL);
+        for(OrderItem orderItem : this.orderItems){
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+        int totalPrice=0;
+        for(OrderItem orderItem : orderItems){
+            totalPrice+=orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -28,4 +28,32 @@ public class OrderItem {
     public void setItem(Item item) {
         this.item = item;
     }
+
+    //==생성 로직==//
+    public OrderItem(Item item, Order order,int orderPrice,int count){
+        this.item=item;
+        this.order=order;
+        this.orderPrice=orderPrice;
+        this.count=count;
+    }
+    /**
+     * 파라미터 없는 생성자 외부 사용 차단
+     */
+    protected OrderItem(){
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 상품 취소
+     */
+    public void cancel(){
+        this.item.AddStock(count);
+    }
+    /**
+     * 상품 총액 계산
+     */
+    public int getTotalPrice(){
+        return orderPrice*count;
+    }
+
 }

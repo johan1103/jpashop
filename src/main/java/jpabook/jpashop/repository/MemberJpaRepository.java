9d7e5c828a769622512491ad119812bd10f34611
johan1103 <johan1103@naver.com>
2023-02-24 17:53:38 +0900

    Edit data-jpa part

--- src/main/java/jpabook/jpashop/repository/MemberJpaRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberJpaRepository.java
@@ -1,7 +1,16 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberJpaRepository extends JpaRepository<Member,Long> {
+
+    List<Member> findByName(String userName);
+    @Query("select m from Member m where m.name =: username")
+    Page<Member> findByUsername(@Param(value = "username")String username);
 }
